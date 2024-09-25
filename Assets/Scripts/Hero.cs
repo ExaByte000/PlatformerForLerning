3ea336338adfe3cfb94a7af2b7ad8cdaacf9e597
Exa Byte <jera8068@gmail.com>
2024-09-25 12:22:49 +0300

    Animations And Camera
    Сделаны Idle, Run, Jump и Fall анимации, так же реализована слежка камеры за игроком

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -10,30 +10,60 @@ public class Hero : MonoBehaviour
     [SerializeField] private int lives = 5;
     [SerializeField] private float jumpForce = 13f;
 
+    private bool isGrounded = true;
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator anim;
 
+    private States State
+    {
+        get { return (States)anim.GetInteger("State"); }
+        set { anim.SetInteger("State", (int)value); }
+    }
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
     }
+    private void FixedUpdate()
+    {
+        CheckGroug();
+    }
     private void Update()
     {
+        if (isGrounded) State = States.Idle;
         if (Input.GetButton("Horizontal")) Run();
-        if (Input.GetButtonDown("Jump")) Jump();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
     }
 
     private void Jump()
     {
+
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 
     private void Run()
     {
+        if (isGrounded) State = States.Run;
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
         sprite.flipX = dir.x < 0f;
     }
+     private void CheckGroug()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
 
+        if (!isGrounded && rb.velocity.y > 0f) State = States.Jump;
+        else if (!isGrounded && rb.velocity.y < 0f) State = States.Fall;
+    }
+}
+
+public enum States
+{
+    Idle,
+    Run,
+    Jump,
+    Fall
 }
